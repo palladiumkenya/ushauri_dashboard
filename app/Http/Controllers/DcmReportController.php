e71d4fe64d79a1de8d4865c07433997b875302da
Ronald-pro <36841157+Ronald-pro@users.noreply.github.com>
2022-06-13 10:49:56 +0300

    fixed bug on dsd reports

--- app/Http/Controllers/DcmReportController.php
+++ app/Http/Controllers/DcmReportController.php
@@ -46,27 +46,28 @@ class DcmReportController extends Controller
 
         if (Auth::user()->access_level == 'Partner') {
             $all_clients_duration_less_well = DcmUnstable::join('tbl_client', 'tbl_client.id', '=', 'tbl_dfc_module.client_id')
+            ->join('tbl_partner_facility', 'tbl_client.mfl_code', '=', 'tbl_partner_facility.mfl_code')
                 ->join('tbl_appointment', 'tbl_client.id', '=', 'tbl_appointment.client_id')
                 ->selectRaw('tbl_client.clinic_number, tbl_client.f_name, tbl_client.m_name, tbl_client.l_name, tbl_dfc_module.duration_less, tbl_appointment.appntmnt_date')
-                ->where('tbl_client.partner_id', Auth::user()->partner_id)
+                ->where('tbl_partner_facility.partner_id', Auth::user()->partner_id)
                 ->where('duration_less', '=', 'Well')->paginate(1000);
 
             $all_clients_duration_less_advanced = DcmUnstable::join('tbl_client', 'tbl_client.id', '=', 'tbl_dfc_module.client_id')
                 ->join('tbl_appointment', 'tbl_client.id', '=', 'tbl_appointment.client_id')
+                ->join('tbl_partner_facility', 'tbl_client.mfl_code', '=', 'tbl_partner_facility.mfl_code')
                 ->selectRaw('tbl_client.clinic_number, tbl_client.f_name, tbl_client.m_name, tbl_client.l_name, tbl_dfc_module.duration_less, tbl_appointment.appntmnt_date')
                 ->where('tbl_client.partner_id', Auth::user()->partner_id)
                 ->where('duration_less', '=', 'Advanced')->paginate(1000);
 
             $all_clients_duration_more_stable = Dcm::select('*')
                 ->where('duration_more', '=', 'Stable')
-                ->where('tbl_client.partner_id', Auth::user()->partner_id)->paginate(1000);
-
-
+                ->where('partner_id', Auth::user()->partner_id)->paginate(1000);
 
             $all_clients_duration_more_unstable = DcmUnstable::join('tbl_client', 'tbl_client.id', '=', 'tbl_dfc_module.client_id')
                 ->join('tbl_appointment', 'tbl_client.id', '=', 'tbl_appointment.client_id')
+                ->join('tbl_partner_facility', 'tbl_client.mfl_code', '=', 'tbl_partner_facility.mfl_code')
                 ->selectRaw('tbl_client.clinic_number, tbl_client.f_name, tbl_client.m_name, tbl_client.l_name, tbl_dfc_module.duration_more, tbl_appointment.appntmnt_date')
-                ->where('tbl_client.partner_id', Auth::user()->partner_id)
+                ->where('tbl_partner_facility.partner_id', Auth::user()->partner_id)
                 ->where('tbl_dfc_module.duration_more', '=', 'Unstable')->paginate(1000);
         }
 
@@ -83,8 +84,6 @@ class DcmReportController extends Controller
 
             $all_clients_duration_more_stable = Dcm::select('*')
                 ->where('duration_more', '=', 'Stable')->paginate(1000);
-
-
 
             $all_clients_duration_more_unstable = Dcm::select('*')
                 ->where('duration_more', '=', 'Unstable')->paginate(1000);
