1afa1e4628b4cf7a3e0ffa93b93c5639c65adcc4
Joshua Murithi <brodiamyzalius@gmail.com>
2024-09-09 15:53:21 +0300

    preping for release

--- app/Http/Controllers/DirectoryController.php
+++ app/Http/Controllers/DirectoryController.php
@@ -48,7 +48,7 @@ class DirectoryController extends Controller
 
         //send this rating value to the facility directory
         try {
-                $apiUrl = "https://art.kenyahmis.org/api/facility/directory/rating";
+                $apiUrl = "https://artrefferal.kenyahmis.org/api/facility/directory/rating";
 
                 $httpresponse = Http::
                                     withoutVerifying()
